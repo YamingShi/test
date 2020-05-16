f8fe284b9d56ba8a975b33e18e900af8517be3f7
Yaming.shi <404304809@qq.com>
2020-05-16 16:24:40 +0800

    输入搜索关键字, 列表显示匹配的结果

--- js/app.js
+++ js/app.js
@@ -17,6 +17,37 @@
 
 $(function () {
 	showHide()
+	hoverSubMenu()
+	search()
+
+	// 3. 输入搜索关键字, 列表显示匹配的结果
+	function search() {
+		$('#txtSearch')
+			.on('keyup focus', function () {
+				var txt = this.value.trim()
+
+				if (txt) {
+					$('#search_helper').show()
+				}
+			})
+			.blur(function () {
+				$('#search_helper').hide()
+			})
+	}
+
+	// 2. 鼠标移动切换二级导航菜单的切换显示和隐藏
+	function hoverSubMenu() {
+		$('#category_items>div.cate_item').hover(
+			function () {
+				console.log(11)
+
+				$(this).children('.sub_cate_box').show()
+			},
+			function () {
+				$(this).children(':last').hide()
+			}
+		)
+	}
 
 	// 1. 鼠标移入显示,移出隐藏
 	function showHide() {
@@ -29,21 +60,6 @@ $(function () {
 			function () {
 				var id = this.id + '_items'
 				$('#' + id).hide()
-			}
-		)
-	}
-
-	// 2. 鼠标移动切换二级导航菜单的切换显示和隐藏
-	hoverSubMenu()
-	function hoverSubMenu() {
-		$('#category_items>div.cate_item').hover(
-			function () {
-				console.log(11)
-
-				$(this).children('.sub_cate_box').show()
-			},
-			function () {
-				$(this).children(':last').hide()
 			}
 		)
 	}
